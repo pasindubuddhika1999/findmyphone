ec7cde8bcf7c96e253c5ebb9178d34791e0f2252
pasinduweerarathne <pasiweerarathne98@gmail.com>
2025-07-23 13:06:39 +0530

    add email

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -630,7 +630,7 @@ const Layout = ({ children }) => {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  <span>findmyphonesrilanka@gmail.com</span>
+                  <span>Lostmobilesrilanka@gmail.com</span>
                 </li>
                 <li className="flex items-start">
                   <svg
